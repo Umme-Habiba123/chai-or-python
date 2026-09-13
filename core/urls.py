25146096e83129c90f818d1d38c8c3fa4d021250
Umme-Habiba123 <habibaislammim@gmail.com>
2026-09-14 02:59:17 +0600

    passing urls by models.model

--- core/urls.py
+++ core/urls.py
@@ -19,6 +19,6 @@ from django.urls import path
 from myapp.views import *
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('', home)
+    path('admin/', admin.site.urls),
+    path('home/', home)
 ]
--- myapp/models.py
+++ myapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class student (models.model):
+class student (models.Model):
     name = models.CharField(max_length=255)
     department = models.CharField(max_length=255)
     roll = models.IntegerField()
